f2c4d0c2ad3a3b0b4cdeb2f1d4a62cd063fa7a2d
amirmohammad ghasemi <amirmohammad.ghas3mi@gmail.com>
2022-03-03 09:35:04 +0330

    Remove comments

--- runner.sh
+++ runner.sh
@@ -7,12 +7,6 @@ to_lang=fa
 main_translation=[0][0][0]
 usage=[1]
 
-#  [1]
-#  [0] [1] [2] ...
-#  [0] -> usage
-#  [1] -> usage list without equivalents
-#  [2] -> list of usageus with equivalents
-
 get_raw_translation() {
 	url="https://translate.googleapis.com/translate_a/single?client=gtx&ie=UTF-8&oe=UTF-8&dt=bd&dt=ex&dt=ld&dt=md&dt=rw&dt=rm&dt=ss&dt=t&dt=at&dt=gt&dt=qca&sl=${from_lang}&tl=${to_lang}&hl=en"
 	curl -G --data-urlencode q="${1}" "${url}" 2> /dev/null
